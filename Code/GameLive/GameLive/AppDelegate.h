79001bb20d6cc161f674904a3d39c7475c252ed9
ios-xiaokai <lixi@lixi.local>
2016-06-26 14:44:49 +0800

    游戏直播项目搭建
    测试上传

---
+++ Code/GameLive/GameLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameLive
+//
+//  Created by lixi on 16/6/26.
+//  Copyright © 2016年 lixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Code/GameLive/GameLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameLive
+//
+//  Created by lixi on 16/6/26.
+//  Copyright © 2016年 lixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
