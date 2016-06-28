d5f2b9b49f4484b3d7598ffeb78bd88162ef7687
lixi <1595607510@qq.com>
2016-06-28 21:22:44 +0800

    还差头部滚动视图

--- Code/GameLive/TRProject/ViewController/HomePageCollectionViewController.m
+++ Code/GameLive/TRProject/ViewController/HomePageCollectionViewController.m
@@ -12,9 +12,10 @@
 #import "HomePageViewModel.h"
 #import "DetailLiveCollectionViewController.h"
 #import "TopCollectionReusableView.h"
-#import "SmallCollectionViewCell.h"
-@interface HomePageCollectionViewController ()<UICollectionViewDelegateFlowLayout>
+
+@interface HomePageCollectionViewController ()<iCarouselDelegate, iCarouselDataSource>
 @property (nonatomic) HomePageViewModel *homePageVM;
+@property (nonatomic) iCarousel *ic;
 
 @end
 
@@ -37,11 +38,13 @@ static NSString * const reuseIdentifier = @"Cell";
     
     [self.collectionView registerNib:[UINib nibWithNibName:@"FirstCollectionReusableView" bundle:nil] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader  withReuseIdentifier:@"lixi"];
     
-    [self.collectionView registerClass:[TopCollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader  withReuseIdentifier:@"lixi2"];
+    [self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader  withReuseIdentifier:@"lixi2"];
     
     [self.collectionView registerNib:[UINib nibWithNibName:@"DetailLiveCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:reuseIdentifier];
     
-    [self.collectionView registerNib:[UINib nibWithNibName:@"SmallCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"lixi3"];
+    //    [self.collectionView registerNib:[UINib nibWithNibName:@"SmallCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"lixi3"];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"first"];
     
     self.collectionView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
         [self.homePageVM getDataWithRequestMode:VMRequestModeRefresh completionHandler:^(NSError *error) {
@@ -50,7 +53,7 @@ static NSString * const reuseIdentifier = @"Cell";
                 DDLogError(@"%@",error);
             }else{
                 NSLog(@"%@",self.homePageVM);
-
+                
                 [self.collectionView reloadData];
             }
         }];
@@ -59,7 +62,10 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 -(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.section == 0) {
-        TopCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"lixi2" forIndexPath:indexPath];
+        UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"lixi2" forIndexPath:indexPath];
+        UIView *iv = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 0, 100)];
+        iv.backgroundColor = [UIColor yellowColor];
+        [headerView addSubview:iv];
         return headerView;
     }else{
         FirstCollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"lixi" forIndexPath:indexPath];
@@ -97,7 +103,8 @@ static NSString * const reuseIdentifier = @"Cell";
     if (section == 0) {
         return CGSizeMake(1, 150);
     }else{
-        return CGSizeMake(300, 40);}
+        return CGSizeMake(300, 40);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -115,7 +122,7 @@ static NSString * const reuseIdentifier = @"Cell";
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
     if (section == 0) {
-        return 7;
+        return 1;
     }else if (section == 1){
         return 2;
     }else{
@@ -126,9 +133,12 @@ static NSString * const reuseIdentifier = @"Cell";
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     
     if (indexPath.section == 0) {
-        SmallCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"lixi3" forIndexPath:indexPath];
-        [cell.iv sd_setImageWithURL:[self.homePageVM smallURLForRow:indexPath.row]];
-        cell.text.text = @"lixi";
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"first" forIndexPath:indexPath];
+        //        cell.backgroundColor = [UIColor redColor];
+        [cell.contentView addSubview:self.ic];
+        [self.ic mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(0);
+        }];
         return cell;
     }else{
         DetailLiveCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
@@ -139,14 +149,19 @@ static NSString * const reuseIdentifier = @"Cell";
         cell.viewNum.text = [self.homePageVM viewForRow:indexPath.row section:indexPath.section];
         return cell;
     }
-//    return [UICollectionViewCell new];
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
-    CGFloat w2 = lround(([UIScreen mainScreen].bounds.size.width - 30) / 2);
-    //156*100
-    CGFloat h2 = w2 * 100 / 156 + 40;
-    return CGSizeMake(w2, h2);
+    if (indexPath.section == 0) {
+        CGFloat w2 = (lround([UIScreen mainScreen].bounds.size.width));
+        CGFloat h2 = 60 ;
+        return CGSizeMake(w2, h2);
+    }else{
+        CGFloat w2 = lround(([UIScreen mainScreen].bounds.size.width - 30) / 2);
+        //156*100
+        CGFloat h2 = w2 * 100 / 156 + 40;
+        return CGSizeMake(w2, h2);
+    }
 }
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
     return UIEdgeInsetsMake(10, 10, 10, 10);
@@ -166,4 +181,56 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 
 
+- (iCarousel *)ic {
+    if(_ic == nil) {
+        _ic = [[iCarousel alloc] initWithFrame:CGRectMake(0, 0, 0, 60)];
+        _ic.delegate = self;
+        _ic.dataSource = self;
+        _ic.autoscroll = 0;
+        _ic.scrollSpeed = .1;
+        _ic.tag = 100;
+    }
+    return _ic;
+}
+
+- (NSInteger)numberOfItemsInCarousel:(iCarousel *)carousel{
+    return 7;
+}
+- (UIView *)carousel:(iCarousel *)carousel viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view{
+    if (!view) {
+        view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 60, 60)];
+        UIImageView *iv = [UIImageView new];
+        iv.tag = 100;
+        [view addSubview:iv];
+        [iv mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.height.width.equalTo(40);
+            make.top.equalTo(0);
+            make.centerX.equalTo(0);
+        }];
+        iv.contentMode = UIViewContentModeScaleAspectFill;
+        iv.layer.cornerRadius = 20;
+        iv.clipsToBounds = YES;
+        UILabel *lab = [UILabel new];
+        lab.tag = 101;
+        [view addSubview:lab];
+        [lab mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(iv.mas_bottom);
+            make.centerX.equalTo(0);
+        }];
+        lab.font = [UIFont systemFontOfSize:10];
+        lab.textColor = [UIColor colorWithRed:50/255.0 green:50/255.0 blue:50/255.0 alpha:1.0];
+    }
+    UILabel *lab = (UILabel *)[view viewWithTag:101];
+    lab.text = [self.homePageVM smallTitleForRow:index];
+    UIImageView *iv = (UIImageView *)[view viewWithTag:100];
+    [iv sd_setImageWithURL:[self.homePageVM smallURLForRow:index]];
+    iv.clipsToBounds = YES;
+    return view;
+}
+- (CGFloat)carousel:(iCarousel *)carousel valueForOption:(iCarouselOption)option withDefault:(CGFloat)value{
+    if (option == iCarouselOptionWrap) {
+        value = YES;
+    }
+    return value;
+}
 @end
